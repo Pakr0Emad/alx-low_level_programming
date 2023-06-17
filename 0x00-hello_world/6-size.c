d1c547860eedbdf29c6ff032fe095345ac302765
//Pakr0Emad <pakr.emad36@gmail.com>
2023-06-17 15:16:01 -0700

    asd

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,11 +10,10 @@
 
 int main(void)
 {
-printf("Size of a char: %u byts(s)\n", sizeof(char));
-printf("Size of an int: %u byts(s)\n", sizeof(int));
-printf("Size of a long int: %u byts(s)\n", sizeof(long int));
-printf("Size of a long long int: %u byts(s)\n", sizeof(long long int));
-printf("Size of a float: %u byts(s)\n", sizeof(float));
-return (0);
-
+	printf("Size of a char: %u byts(s)\n", sizeof(char));
+	printf("Size of an int: %u byts(s)\n", sizeof(int));
+	printf("Size of a long int: %u byts(s)\n", sizeof(long int));
+	printf("Size of a long long int: %u byts(s)\n", sizeof(long long int));
+	printf("Size of a float: %u byts(s)\n", sizeof(float));
+	return (0);
 }
