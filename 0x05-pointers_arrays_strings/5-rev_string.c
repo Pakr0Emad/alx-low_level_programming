b60ef59762a863b07f989484e34afa555d26c1ec
//Pakr0Emad <pakr.emad36@gmail.com>
2023-06-27 12:21:43 -0700

    asd

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * rev_string - check the code for Holberton School students.
+ *
+ * @s: string
+ *
+ * Return: Always 0.
+ */
+
+void rev_string(char *s)
+{
+	int l, i;
+
+	char temp;
+	/*find string*/
+	for (l = 0; s[l] != '\n'; ++l)
+		;
+
+	/*swap the string*/
+	for (i = 0; i < l / 2; ++l)
+		;
+	{
+		temp = s[i];
+		s[i] = s[l - 1 - i]; /*because the array starts from 0*/
+		s[l - 1 - i] = temp;
+	}
+}
