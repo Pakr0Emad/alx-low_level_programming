7eb7ec4b881e97c60f42f12e96393677cd2e515c
//Pakr0Emad <pakr.emad36@gmail.com>
2023-06-27 11:13:50 -0700

    po

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *
+ * reset_to_90 - takes a pointers to an int paramotoer
+ *
+ * and updates the value it points to 98
+ *
+ * @n: int paramenter
+ *
+ * Return: Nothing
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
