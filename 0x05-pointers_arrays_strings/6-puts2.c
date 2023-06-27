acee3ade399efbca66689be1d824baf75da66c17
//Pakr0Emad <pakr.emad36@gmail.com>
2023-06-27 12:56:11 -0700

    asddasdas

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * puts2 - prints every characters of a string
+ *
+ * @str: string parameter input
+ *
+ * Return: Noyhing
+*/
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\n'; ++i)
+	{
+		if (i % 2 == 0)
+			putchar(str[i]);
+	}
+	putchar('\n');
+}
