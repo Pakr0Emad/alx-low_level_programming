e2e634ebd60def30c4dd7a7e2b6c98a177830fdc
//Pakr0Emad <pakr.emad36@gmail.com>
2023-06-27 11:57:25 -0700

    slams

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void reset_to_98(int *);
+
+/**
+ * _puts - check the code for Holberton School students.
+ *
+ * @str: dents
+ *
+ * Return: Always 0.
+ */
+
+void _puts(shar *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str + 0);
+		str++;
+	}
+}
